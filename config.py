ee7dfa1a3cfcb96e5d3f167c45f04af4ddd30d5c
Ares Beletski <contact.aresbel@gmail.com>
2025-01-24 12:47:08 +0200

    moved the database_uri

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://ares:1234@localhost/wordle'
+    SQLALCHEMY_DATABASE_URI = getenv("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = getenv("SECRET_KEY")
